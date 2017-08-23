1701ae309a1e2d93d69bad9c2b2b79619508f6ea
dreamszl <chenao3220@gmail.com>
2017-08-23 18:59:00 +0800

    Add eclim support

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -3,4 +3,32 @@
       org-directory "~/Documents/org"
       exec-path-from-shell-arguments '("-i"))
 
+;;====================================================
+;; eclim mode settings
+;;====================================================
+(require 'eclim)
+(setq eclimd-autostart t)
+
+(defun my-java-mode-hook ()
+  (eclim-mode t))
+
+(add-hook 'java-mode-hook 'my-java-mode-hook)
+
+(custom-set-variables
+ '(eclim-eclipse-dirs '("~/softwares/eclipse"))
+ '(eclim-executable "~/softwares/eclipse/eclim")
+ '(eclimd-default-workspace "~/softwares/eclipse"))
+
+(require 'company-emacs-eclim)
+(company-emacs-eclim-setup)
+(global-company-mode t)
+
+;;====================================================
+;; company idle completion settings
+;;====================================================
+
+(setq company-minimum-prefix-length 2)
+(setq company-idle-delay 0)
+;;====================================================
+
 (provide 'init-local)
