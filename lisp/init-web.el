ec9a3ab08403ed38879a46dbae98852e99e0d186
dreamszl <chenao3220@gmail.com>
2017-09-29 21:23:41 +0800

    Add jsx support

--- lisp/init-web.el
+++ lisp/init-web.el
@@ -2,6 +2,12 @@
 
 (require 'web-mode)
 
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
+(setq web-mode-code-indent-offset 2)
+(setq web-mode-content-types-alist
+      '(("jsx" . "\\.js[x]?\\'")))
+
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
@@ -11,10 +17,8 @@
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.vue\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js[x]\\'" . web-mode))
 
-(setq web-mode-markup-indent-offset 2)
-(setq web-mode-css-indent-offset 2)
-(setq web-mode-code-indent-offset 2)
 (set-face-attribute 'web-mode-html-tag-face nil :foreground "chocolate3")
 (set-face-attribute 'web-mode-html-attr-name-face nil :foreground "YellowGreen")
 (set-face-attribute 'web-mode-html-attr-equal-face nil :foreground "white")
